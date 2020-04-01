03612a4eb090858e8b51c977bc5ce7b630688a55
Viviaana <kjingham91@gmail.com>
2020-04-01 11:37:53 +0100

    added tickboxes

--- models/user.js
+++ models/user.js
@@ -15,6 +15,31 @@ const userSchema = new mongoose.Schema({
     type: String,
     required: true,
     minlength: 6
+  },
+  volunteer: {
+    type: Boolean,
+    required: false,
+    isChecked: true
+  },
+  notvolunteer: {
+    type: Boolean,
+    required: false,
+    isChecked: true
+  },
+  shopping: {
+    type: Boolean,
+    required: false,
+    isChecked: true
+  },
+  medicine: {
+    type: Boolean,
+    required: false,
+    isChecked: true
+  },
+  chat: {
+    type: Boolean,
+    required: false,
+    isChecked: true
   }
 });
 
